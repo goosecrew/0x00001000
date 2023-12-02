236c210e937e5b051fb4d7ecf87feea95f0ef6ea
pgusev <pgusev@team.amocrm.com>
2023-12-02 16:25:18 +0300

    initial

---
+++ src/settings.h
@@ -0,0 +1,9 @@
+#define LED_PIN 2
+#define WIFI_SSID "ssid"
+#define WIFI_PASSWORD "wifipass"
+
+#define AUTHOR_EMAIL "author@example.com"
+#define AUTHOR_PASSWORD "pass"
+#define SMTP_HOST "smtp.yandex.ru"
+#define SMTP_PORT 465
+#define RECIPIENT_EMAIL "recipient@gmail.com"
